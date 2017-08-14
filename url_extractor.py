0f0f1c5217e5c46456bac597f2f629b23604eccb
shad0wrunner <shadowrunner3k@yandex.ru>
2017-08-14 13:33:15 +0300

    Small style corrections

--- url_extractor.py
+++ url_extractor.py
@@ -10,52 +10,58 @@ def main():
     The tool is intended to extract all links (complete and relative ones) from HTML tag attributes
     TODO: urljoin
     """
+
+    # Retrieving parameters
     links = []
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', '--method', action='store', help='What method to use in request')
     parser.add_argument('-u', '--url', action='store', help='URL to call with the specified method')
 
     try:
+        # Defining and splitting variables from the incoming url
         args = parser.parse_args()
         url = args.url
         scheme = urlparse(url).scheme
         method = args.method
 
+        # checking for the supported methods
         if not method.upper() in ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE']:
             raise ValueError("Method %s is not supported." % method)
 
+        # requesting the url
         response = requests.request(method, url, verify=False)  # get the response
         url = response.url.rstrip('/')  # if there was a redirect - acquire the URL from the response
 
         print('[+] Retrieving ' + url)
         raw_html = response.content
-
-        soup = BeautifulSoup(raw_html, "html.parser")
+        parsed_html = BeautifulSoup(raw_html, "html.parser")
 
         # neat print headers
         print("[+] Received the response HTTP %d" % response.status_code)
         for header in response.headers:
             print(header + ':', response.headers[header])
 
-        # gathering a list of href's and src's
-        for script in soup.select('script'):
+        # gathering a list of links from specific elements
+        for script in parsed_html.select('script'):
             links.append(script.get('src'))
-        for anchor in soup.select('a'):
+        for anchor in parsed_html.select('a'):
             links.append(anchor.get('href'))
-        for link in soup.select('link'):
+        for link in parsed_html.select('link'):
             links.append(link.get('href'))
-        for form in soup.select('form'):
+        for form in parsed_html.select('form'):
             links.append(form.get('action'))
 
+        # removing bookmarks, emails, skype and '/'
         links = [link for link in links if
                  not link[0] == '#' and
                  not urlparse(link).scheme in ['mailto', 'skype'] and
-                 not link == '/']  # removing bookmarks, emails and skype and '/'
+                 not link == '/']
 
+        # appending URL value based on the link presentation
         links = [scheme + '://' + link[2:] if link[0:2] == '//' else
                  link if urlparse(link).scheme in ['http', 'https'] else
                  url + link if link[0] == '/' else
-                 url + '/' + link for link in links]  # appending URL value
+                 url + '/' + link for link in links]
 
     except Exception as e:
         print("[-] Something went wrong: %s" % e)
@@ -63,11 +69,11 @@ def main():
     except KeyboardInterrupt:
         print("[x] Exiting by user command")
 
+    # final links count and listing
     print("\n[+] %d Links extracted:" % len(links))
     for link in links:
         print(link)
     return links
-
 
 if __name__ == "__main__":
     print('[*] Starting the main module')
