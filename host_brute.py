dd98f3deecacd9d4940ca7f248051d51c3b25526
shad0wrunner <shadowrunner3k@yandex.ru>
2017-08-23 21:18:53 +0300

    parser moved out of main

--- host_brute.py
+++ host_brute.py
@@ -7,37 +7,39 @@ import string
 import timeit
 
 
-def progress(current_position, total, percentage):
-    divisor = total // percentage
-    if current_position % divisor == 0:
+def parse_parameters():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--method', action='store', help='What method to use in request', default='GET')
+    parser.add_argument('-s', '--schema', action='store', help='Schema - http, https, etc.', default='http')
+    parser.add_argument('-d', '--domain', action='store', help='Domain against which brute-force subdomains')
+    parser.add_argument('-l', '--length', action='store', type=int, help='Length of the subdomains names')
+    parser.add_argument('-o', '--outfile', action='store', help='Output file for the working links')
+    args = parser.parse_args()
+    parsed_args = (args.method, args.schema, args.domain, args.length)
+    return parsed_args
+
+
+def progress(current_position, total, resolution):
+    part = total*resolution // 100
+    if current_position % part == 0:
         print('%d percent complete (%d out of %d)' % ((current_position * 100 / total),
                                                       current_position,
                                                       total))
 
 
-def main():
+def main(method, schema, domain, subdomain_length):
     """
     brute-forces subdomains
     """
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--method', action='store', help='What method to use in request', default='GET')
-    parser.add_argument('-d', '--domain', action='store', help='Domain against which brute-force subdomains')
-    parser.add_argument('-o', '--outfile', action='store', help='Output file for the working links')
-    parser.add_argument('-l', '--length', action='store', type=int, help='Length of the subdomains names')
-    parser.add_argument('-s', '--schema', action='store', help='Schema - http, https, etc.', default='http')
     try:
-        args = parser.parse_args()
-        method = args.method
-        schema, domain = args.schema, args.domain
-        subdomain_length = args.length
         suspicious_urls, working_urls = [], []
         failed_urls_count = 0
         requests.packages.urllib3.disable_warnings()  # suppressing unsafe HTTPS warnings
 
         dns_resolver = dns.resolver.Resolver()
 
-        # specifies how often should the progress be updated [1-100]. 20 - around each 5% of requests.
-        progress_freq = 50
+        # specifies how often (in percent) should the progress be updated.
+        progress_resolution = 0.5
 
         char_set = string.ascii_lowercase + string.digits + '-' + '_'
 
@@ -54,11 +56,10 @@ def main():
         print('[+] Brute-forcing subdomains. %d URLs in total' % total_hosts)
         for index, host in enumerate(host_generator):
             try:
-                progress(index, total_hosts, progress_freq)
-                dns_response = (dns_resolver.query(host, 'A', raise_on_no_answer=False))
-                resolved_host = dns_response.qname.to_text(omit_final_dot=True)
+                progress(index, total_hosts, progress_resolution)
+                dns_response = (dns_resolver.query(host, 'A'))
 
-                url = schema+'://'+resolved_host
+                url = schema+'://'+dns_response.qname.to_text(omit_final_dot=True)
                 http_response = requests.request(method, url, verify=False, timeout=(3, 3))  # get the response
                 print('%s answers HTTP %d' % (url, http_response.status_code))
                 working_urls.append((url, http_response.status_code))
@@ -90,4 +91,5 @@ def main():
 
 if __name__ == '__main__':
     print('[+] Starting the main module')
-    main()
+    parsed_parameters = parse_parameters()
+    main(*parsed_parameters)
